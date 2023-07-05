b0874a124bf1442781dd892198e0da48e185cec0
rtexelm <texelist@gmail.com>
2023-07-05 13:32:58 -0500

    Add game initiation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 use piston_window::*;
-// mod game;
+mod game;
 // mod drawing;
-// use game::Game;
+use game::Game;
 
 const WIDTH: f64 = 800.0;
 const HEIGHT: f64 = 600.0;
@@ -14,4 +14,19 @@ fn main() {
 
     let (width, height) = (640.0, 480.0);
     let mut game = Game::new(width, height);
+
+    let mut events = window.events;
+    while let Some(event) = events.next(&mut window) {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+
+        if let Some(args) = event.update_args() {
+            game.update(args.dt);
+        }
+
+        if let Some(_args) = event.render_args() {
+            window.draw_2d(&event, |c, g, _| game.draw(&c, g));
+        }
+    }
 }
